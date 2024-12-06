9651a7033f924f211c020e1321c72ddf298d3360
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-12-06 11:22:33 +0000

    chore: add seeder
    More applications and events will be added in the next commit

---
+++ scripts/seed.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+
+const { authenticate, close } = require('../lib/sequelize');
+const logger = require('../lib/logger');
+const {
+    Application,
+    Event
+} = require('../models');
+
+const seedStatePath = path.resolve(__dirname, '../state/.seed-executed-' + (process.env.NODE_ENV || 'development'));
+
+const data = {};
+
+async function createEvents() {
+    const events = [];
+
+    events.published = await Event.create({
+        name: 'Published Event',
+        url: 'published-event',
+        description: 'This is a published event.',
+        application_starts: new Date(),
+        application_ends: moment().add(1, 'week').toDate(),
+        starts: moment().add(2, 'weeks').toDate(),
+        ends: moment().add(3, 'weeks').toDate(),
+        fee: 50.00,
+        organizing_bodies: [{ body_id: 1, body_name: 'AEGEE-Antenna' }],
+        locations: [{ name: 'Location', position: { lat: 0, lng: 0 } }],
+        type: 'training',
+        status: 'published',
+        organizers: [{ user_id: 1, first_name: 'Admin', last_name: 'Admin' }],
+        questions: [{ description: 'Why do you want to participate?', type: 'text', required: false }],
+        meals_per_day: 2,
+        accommodation_type: 'none',
+        budget: 'https://online-event.example.com',
+        programme: 'https://online-event.example.com',
+    });
+
+    return events;
+}
+
+async function createApplications() {
+    const applications = [];
+
+    applications.first = await Application.create({
+        user_id: 2,
+        event_id: data.events.published.id,
+        body_id: 1,
+        first_name: 'A',
+        last_name: 'Member',
+        body_name: 'AEGEE-Antenna',
+        status: 'pending',
+        confirmed: false,
+        attended: false,
+        answers: ['Because I want to learn new things.'],
+        agreed_to_privacy_policy: true,
+    });
+
+    return applications;
+}
+
+if (fs.existsSync(seedStatePath)) {
+    logger.info('[seeds         ]: Seed was executed already, no need to run it again.');
+    process.exit(0);
+}
+
+if (process.env.NODE_ENV === 'production') {
+    logger.error('Not running seeds in production.');
+    process.exit(0);
+}
+
+authenticate().then(async () => {
+    logger.info('[seeds         ]: DB connected');
+
+    logger.info('[seeds         ]: Create events');
+    data.events = await createEvents();
+
+    logger.info('[seeds         ]: Create applications');
+    data.applications = await createApplications();
+
+    await close();
+}).catch((err) => {
+    logger.error(`[seeds         ]: Seed creation error: ${err}`);
+    process.exit(1);
+});
